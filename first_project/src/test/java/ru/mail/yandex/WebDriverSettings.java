98e625f122dfbe925c011f1644a5126eda419bed
Dragunov-R <dragunov_roman@icloud.com>
2020-12-07 16:17:20 +0700

    v2

--- first_project/src/test/java/ru/mail/yandex/WebDriverSettings.java
+++ first_project/src/test/java/ru/mail/yandex/WebDriverSettings.java
@@ -4,6 +4,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class WebDriverSettings {
 
     public ChromeDriver driver;
@@ -12,6 +14,7 @@ public class WebDriverSettings {
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "/Users/romandragunov/WebDrivers/chromedriver");
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @After
